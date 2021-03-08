a007e28661ac1c0e0fdc0e16114bf4cab9fa92fd
İmdat Çakmak <65205036+imdatcakmak@users.noreply.github.com>
2021-03-08 16:44:55 +0300

    Add files via upload

---
+++ BLLDers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EntityLayer;
+using DataAccessLayer;
+namespace BusinessLogicLayer
+{
+   public  class BLLDers
+    {
+        public static List<EntityDers> BllListele()
+        {
+            return DALDers.DersListesi();
+        }
+
+        public static int TalepEkleBLL(EntityBasvuruForm p)
+        {
+            if(p.BASOGRID!=null && p.BASDERSID != null)
+            {
+                return DALDers.TalepEkle(p);
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ BLLOgrenci.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using EntityLayer;
+using DataAccessLayer;
+using System.Collections.Generic;
+
+namespace BusinessLogicLayer
+{
+    public class BLLOgrenci
+    {
+        public static int OgrenciEkleBLL(EntityOgrenci p)
+        {
+            if(p.AD!=null && p.SOYAD!=null && p.NUMARA!=null && p.SIFRE!=null && p.FOTOGRAF != null)
+            {
+                return DALOgrenci.OgrenciEkle(p);
+            }
+            return -1;
+        }
+
+        public static List<EntityOgrenci> BllListele()
+        {
+            return DALOgrenci.OgrenciListesi();
+        }
+        public static bool OgrenciSilBLL(int p)
+        {
+            if (p >= 0)
+            {
+                return DALOgrenci.OgrenciSil(p);
+            }
+            return false;
+        }
+        public static List<EntityOgrenci> BllDetay(int p)
+        {
+            return DALOgrenci.OgrenciDetay(p);
+        }
+
+        public static bool OgrenciGuncelleBLL(EntityOgrenci p)
+        {
+            if (p.AD != null && p.AD!="" && p.SOYAD != null && p.SOYAD!="" && p.NUMARA != null && p.NUMARA!="" && p.SIFRE != null && p.SIFRE!="" && p.FOTOGRAF != null && p.FOTOGRAF!="" && p.ID>0)
+            {
+                return DALOgrenci.OgrenciGuncelle(p);
+            }
+            return false;
+        }
+    }
+}
